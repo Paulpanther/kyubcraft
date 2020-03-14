fbc5bc1ec73a5908b8f845ca42500dd91f49aa0b
Paul Methfessel <paul.methfessel@web.de>
2020-03-14 20:04:43 +0100

    Add scale to increase size for kyub

--- webpage/src/index.ts
+++ webpage/src/index.ts
@@ -1,19 +1,31 @@
-import {Mesh, BoxGeometry, Geometry} from 'three';
+import {Mesh, BoxGeometry, Geometry, Vector3} from 'three';
 import {saveAs} from 'file-saver';
 import * as exportSTL from 'threejs-export-stl';
 
+// Object needs to be scaled for Kyub
+// 1 = 1mm
+const SCALE = 100;
+
 function toBoxes(encoded: string): Geometry[] {
-    return encoded
+    const points = encoded
         .split(';')  // Get separate points
         .map(point => point.split('.'))  // Split coordinates of point
         .filter(point => point.length === 3
             && point.every(number => !isNaN(Number(number)) && number !== ''))  // Check point is valid
         .map(point => {
             const numbers = point.map(Number);
-            const box = new BoxGeometry(1, 1, 1);
-            box.translate(numbers[0], numbers[2], numbers[1]);  // Swap y and z because minecraft has a "special" system
-            return box;
+            // Swap y and z because minecraft has a "special" system
+            return new Vector3(numbers[0], numbers[2], numbers[1]);
         });
+    const boxes = points.map(point => {
+        const box = new BoxGeometry(1, 1, 1);
+        box.translate(point.x, point.y, point.z);
+        box.scale(SCALE, SCALE, SCALE);
+        return box;
+    });
+    // const center = boxes
+    //     .reduce((prev, current) => prev.clone().add(current.center()), new Vector3());
+    return boxes;
 }
 
 function mergeIntoMesh(boxes: Geometry[]): Mesh {
@@ -26,7 +38,7 @@ function mergeIntoMesh(boxes: Geometry[]): Mesh {
 }
 
 function showSavePrompt(mesh: Mesh) {
-    const buffer = exportSTL.fromMesh(mesh);
+    const buffer = exportSTL.fromMesh(mesh, false);
     const blob = new Blob([buffer], {type: exportSTL.mimeType});
 
     saveAs(blob, 'kyub.stl');
